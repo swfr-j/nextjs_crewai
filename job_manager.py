07c04950bb63c8d1142680ab55bc2b2cef9a6d30
swfr-j <130271263+swfr-j@users.noreply.github.com>
2024-07-25 15:40:14 +0530

    Add thread safety to job manager

--- job_manager.py
+++ job_manager.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from dataclasses import dataclass
 from typing import List, Dict
+from threading import Lock
 
 
 @dataclass
@@ -14,3 +15,27 @@ class Job:
     status: str
     events: List[Event]
     results: str
+
+
+jobs_lock = Lock()
+jobs: Dict[str, Job] = {}
+
+
+def append_event(job_id: str, event_data: str):
+    with jobs_lock:
+        if job_id not in jobs:
+            print(f"Starting Job: {job_id}")
+            jobs[job_id] = Job(
+                status="STARTED",
+                events=[],
+                results="",
+            )
+
+        else:
+            print(f"Appending event to Job: {job_id}")
+            jobs[job_id].events.append(
+                Event(
+                    timestamp=datetime.now(),
+                    message=event_data,
+                )
+            )
